e5304593916d3b5ad0d7a50baad5f182bb333113
honaldu <hsh9636@gmail.com>
2020-08-14 22:05:41 +0900

    sideNav 모듈 작성

--- frontend/src/modules/sideNav.js
+++ frontend/src/modules/sideNav.js
@@ -5,3 +5,39 @@ const SET_SIDENAV_FAILURE = "sideNav/SET_SIDENAV_SUCCESS";
 export const setSideNav = () => ({
   type: SET_SIDENAV,
 });
+export const setSideNavSuccess = (data) => ({
+  type: SET_SIDENAV_SUCCESS,
+  data,
+});
+export const setSideNavFailure = (error) => ({
+  type: SET_SIDENAV_FAILURE,
+  error,
+});
+
+const initialState = {
+  categories: null,
+  cateError: null,
+  loading: false,
+};
+
+function sideNav(state = initialState, action) {
+  switch (action.type) {
+    case SET_SIDENAV:
+      return {
+        ...state,
+        loading: true,
+      };
+    case SET_SIDENAV_SUCCESS:
+      return {
+        ...state,
+        categories: action.data,
+      };
+    case SET_SIDENAV_FAILURE:
+      return {
+        ...state,
+        cateError: action.error,
+      };
+    default:
+      return state;
+  }
+}
